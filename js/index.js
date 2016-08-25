f58517e5deb01a5649bab0ee515ba2977742811c
JrodManU <jrodmanu@yahoo.com>
2016-08-24 22:45:03 -0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+  var source = $("#yearsTemplate").html();
+  var template = Handlebars.compile(source);
+
+  var data = {years: [
+      {
+        yearName: "1776",
+        events: [
+          {
+            class: "danger",
+            eventName: "JFK DIED",
+            eventDesc: "he died"
+          },
+        ]
+      },
+      {
+        yearName: "1779",
+        events: [
+          {
+            class: "info",
+            eventName: "Abe Lincoln lied",
+            eventDesc: "he died"
+          },
+          {
+            class: "danger",
+            eventName: "Abe Lincoln lied again",
+            eventDesc: "he died.. again"
+          },
+        ]
+      },
+  ]};
+
+  $("#mainContainer").html(template(data));
+});
